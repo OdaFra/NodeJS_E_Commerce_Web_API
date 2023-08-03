0bf5eba7e190034dac1e372056590bab48d562ed
Oscar Ramirez <oscar.ramirez@tigo.net.py>
2023-08-03 18:43:04 -0400

    feat: Agreamos express.json para parsear los json como las pruebas de .get y post

--- app.js
+++ app.js
@@ -6,8 +6,22 @@ require('dotenv/config');
 
 const api = process.env.API_URL;
 
-app.get('/', (req, res)=>{
-res.send('Hello Api.....!')
+//Milddware
+
+app.use(express.json())
+
+app.get(`${api}/products`, (req, res)=>{
+    const products ={
+        id: 1, 
+        name: 'Notebook',
+        iamge:'notebook_url'
+    }
+res.send(products)
+});
+
+app.post(`${api}/products`, (req, res)=>{
+    const newProduct = req.body
+res.send(newProduct)
 });
 
 app.listen(3000,() =>{
