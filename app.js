9a5502853b2da47d47adf8d6aa2fe3f69c0d64cf
Oscar Ramirez <osramirezf@gmail.com>
2023-09-04 23:05:38 -0400

    refactor: We apply refactor

--- app.js
+++ app.js
@@ -1,28 +1,14 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-
 const mongoose = require("mongoose");
 
 require("dotenv/config");
 
+const productsRouter = require('./routers/products');
+
 const api = process.env.API_URL;
 const connectMongodb = process.env.CONNECTION_STRING;
-
-//Generar schema para mongosee
-
-const productSchema = mongoose.Schema({
-  name: String,
-  image: String,
-  countInStock: {
-    type: Number,
-    required: true
-  },
-})
-
-//En base al schema generado, creamos el modelo
-
-const Product = mongoose.model('Products', productSchema);
 
 //Milddware
 //Instancia necesaria para aplicar parseo de json
@@ -30,48 +16,16 @@ const Product = mongoose.model('Products', productSchema);
 app.use(express.json());
 app.use(morgan("tiny"));
 
+
+//Routers
+app.use(`${api}/products`, productsRouter);
+
 mongoose.connect(connectMongodb).then(() => {
   console.log('Database Connection is ready..!')
 }).catch((err) => {
   console.log(err)
 });
 
-app.get(`${api}/products`, async (req, res) =>  {
-  const productList = await Product.find()
-  if(!productList){
-    res.status(500).json({
-      success:false
-    })
-  }
-  res.send(productList)
-  // const products = {
-  //   id: 1,
-  //   name: "Notebook",
-  //   precio: 1200,
-  //   iamge: "notebook_url",
-  // };
-  //res.send(products);
-});
-
-app.post(`${api}/products`, (req, res) => {
-  const product = new Product({
-    name: req.body.name,
-    image: req.body.image,
-    countInStock: req.body.countInStock,
-  })
-  product.save().then((createdProduct=>{
-    res.status(201).json(createdProduct);
-  })).catch((err)=>{
-    res.status(500).json(
-      {
-        error: err,
-        success: false
-      }
-    )
-  });
-  // const newProduct = req.body;
-  // res.send(newProduct);
-});
 
 app.listen(3000, () => {
   console.log(api);
