7ba503ad1d7fdd07dbb7ad04b94a7e5a33ece32f
Oscar Ramirez <oscar.ramirez@tigo.net.py>
2023-09-07 18:08:18 -0400

    refactor: Aplicamos refactor para el routers in category

--- routers/categories.js
+++ routers/categories.js
@@ -1,14 +1,29 @@
-const {Category} = require('../models/category');
-const express = require('express');
+const { Category } = require("../models/category");
+const express = require("express");
 const router = express.Router();
 
-router.get(`/`, async (req, res) =>{
-    const categoryList = await Category.find();
+router.get(`/`, async (req, res) => {
+  const categoryList = await Category.find();
 
-    if(!categoryList) {
-        res.status(500).json({success: false})
-    } 
-    res.send(categoryList);
-})
+  if (!categoryList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(categoryList);
+});
 
-module.exports =router;
+router.post("/", async (req, res) => {
+  let category = new Category({
+    name: req.body.name,
+    icon: req.body.icon,
+    color: req.body.color,
+  });
+
+  category = await category.save();
+
+  if (!category) 
+    return res.status(404).send("The category cannot be created!");
+
+    res.send(category);
+});
+
+module.exports = router;
