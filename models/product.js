2349937becf1cfc32ab866c83d9d8ce826e41ad1
Oscar Ramirez <oscar.ramirez@tigo.net.py>
2023-09-07 18:07:50 -0400

    refactor: Aplicamos refactor para el schema de product

--- models/product.js
+++ models/product.js
@@ -1,16 +1,65 @@
 const mongoose = require("mongoose");
 
-
-
 //Generar schema para mongosee
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-      type: Number,
-      required: true
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  richDescription: {
+    type: String,
+    default: "",
+  },
+  image: {
+    type: String,
+    default: "",
+  },
+  images: [
+    {
+      type: String,
+      default: "",
     },
-  })
-  
+  ],
+  brand: {
+    type: String,
+    default: ''
+  },
+  price: {
+    type: Number,
+    default: 0
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    min: 0,
+    max: 255,
+  },
+  rating: {
+    type: Number,
+    default: 0
+  },
+  numReviews: {
+    type: String,
+    default: 0
+  },
+  isFeatured: {
+    type: Boolean,
+    default: false
+  },
+  dateCreated: {
+    type: Date,
+    default: Date.now
+  },
+});
+
 //En base al schema generado, creamos el modelo a ser exportado
-exports.Product = mongoose.model('Products', productSchema);
+exports.Product = mongoose.model("Products", productSchema);
