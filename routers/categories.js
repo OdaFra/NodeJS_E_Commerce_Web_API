313e467497333e9f260ed49e0766ec97a9f9c992
Oscar Ramirez <osramirezf@gmail.com>
2023-09-15 22:24:21 -0400

    fix: Corregimos el nombre de la variable de Category a category

--- routers/categories.js
+++ routers/categories.js
@@ -16,13 +16,12 @@ router.get(`/`, async (req, res) => {
 
 router.get("/:id", async (req, res) => {
   const category = await Category.findById(req.params.id);
-
   if (!category) {
     return res.status(500).json({
       message: "The category with the given ID was not found!!",
     });
   }
-  return res.status(200).send(Category);
+  return res.status(200).send(category);
 });
 
 // Add
