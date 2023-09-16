68c36e620590af459b4c26cf38ea514a4a406a82
Oscar Ramirez <osramirezf@gmail.com>
2023-09-15 22:02:53 -0400

    feat: Agregamos metodo en relacion a router.delete para category

--- routers/categories.js
+++ routers/categories.js
@@ -2,6 +2,7 @@ const { Category } = require("../models/category");
 const express = require("express");
 const router = express.Router();
 
+// List or Get
 router.get(`/`, async (req, res) => {
   const categoryList = await Category.find();
 
@@ -11,6 +12,7 @@ router.get(`/`, async (req, res) => {
   res.send(categoryList);
 });
 
+// Add
 router.post("/", async (req, res) => {
   let category = new Category({
     name: req.body.name,
@@ -22,7 +24,31 @@ router.post("/", async (req, res) => {
 
   if (!category) return res.status(404).send("The category cannot be created!");
 
-    res.send(category);
+  res.send(category);
+});
+
+// Delete
+router.delete("/:id", (req, res) => {
+  Category.findByIdAndRemove(req.params.id)
+    .then((category) => {
+      if (category) {
+        return res.status(200).json({
+          success: true,
+          message: "The category is delete",
+        });
+      } else {
+        return res.status(404).json({
+          success: false,
+          message: "Category not found!!!",
+        });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        success: false,
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
