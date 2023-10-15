af33be4d20b1c6982565355a5f7560b65d06c876
Oscar Ramirez <osramirezf@gmail.com>
2023-10-14 22:10:49 -0300

    refactor: Aplicamos refactor para el schema de users.

--- models/user.js
+++ models/user.js
@@ -1,12 +1,56 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  passwordHash: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  isAmin: {
+    type: Boolean,
+    default: true,
+  },
+  street: {
+    type: String,
+    default: "",
+  },
+  apartment: {
+    type: String,
+    default: "",
+  },
+  zip: {
+    type: String,
+    default: "",
+  },
+  city:{
+    type:String,
+    default:'',
+  },
+  country: {
+    type: String,
+    default:''
+  },
+});
 
-exports.User = mongoose.model('User', userSchema);
+userSchema.virtual('id').get(function(){
+    return this._id.toHexString();
+  });
+  
+  userSchema.set('toJSON', {
+    virtuals:true,
+  })
+
+
+exports.User = mongoose.model("User", userSchema);
+exports.userSchema = userSchema;
