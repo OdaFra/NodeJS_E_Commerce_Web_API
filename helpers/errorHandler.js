7fcba6f383130be4c35f4be6c625efc014a25ba2
Oscar Ramirez <osramirezf@gmail.com>
2023-10-28 20:53:12 -0300

    feat: Agregamos function errorHandler para el manejo de errores.

---
+++ helpers/errorHandler.js
@@ -0,0 +1,20 @@
+function errorHandler(err, req, res, next) {
+    // jwt authentication error
+  if (err.name === "UnautorizedError") {
+    res.status(401).json({
+      message: "The user is not autorized!!",
+      error: err,
+    });
+  }
+  //validation error
+  if (err.name === "ValidationError") {
+    res.status(401).json({
+      message: err,
+    });
+  }
+  //default to 500 server error
+  res.status(500).json({
+    message: err,
+  });
+}
+module.exports = errorHandler;
