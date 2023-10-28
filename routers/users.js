a9c50ec00e0c04a9e54b81c08edad1e7d1bbbc33
Oscar Ramirez <osramirezf@gmail.com>
2023-10-28 19:35:22 -0300

    feat: Se agrega router.post para el manejo de autenticacion comparando el pass para responde el email + token.

--- routers/users.js
+++ routers/users.js
@@ -1,12 +1,13 @@
 const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 
 router.get(`/`, async (req, res) => {
-  const userList = await User.find().select('-passwordHash');
-
+  const userList = await User.find().select("-passwordHash");
+ 
   if (!userList) {
     res.status(500).json({ success: false });
   }
@@ -14,7 +15,7 @@ router.get(`/`, async (req, res) => {
 });
 
 router.get("/:id", async (req, res) => {
-  const user = await User.findById(req.params.id).select('-passwordHash');
+  const user = await User.findById(req.params.id).select("-passwordHash");
   if (!user) {
     return res.status(500).json({
       message: "The user with the given ID was not found!!",
@@ -28,7 +29,7 @@ router.post("/", async (req, res) => {
     name: req.body.name,
     email: req.body.email,
     //color:req.body.color,
-    passwordHash: bcrypt.hashSync(req.body.password,10),
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
     apartment: req.body.apartment,
@@ -42,6 +43,41 @@ router.post("/", async (req, res) => {
   if (!user) return res.status(400).send("The user cannot be created!");
 
   res.send(user);
+});
+
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+    password:req.body.passwordHash
+  });
+
+  const secret = process.env.secret;
+
+  if(!user){
+    return res.status(400).send('The user not found!!')
+  } 
+
+  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)){
+
+    const token = jwt.sign(
+      {
+        userId: user.id
+      },
+      secret,
+      {
+        expiresIn: '1d'
+      }
+    )
+
+    res.status(200).send({message:'User Authenticated!!',
+      user: user.email, token: token
+  })
+  }else{
+    res.status(400).send('password is wrong!!')
+  }
+
+  // return res.status(200).send(user);
+
 });
 
 module.exports = router;
