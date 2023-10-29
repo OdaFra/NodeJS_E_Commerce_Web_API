f68f40126f177c9c1b01c2b17ad2e3b1b5f83e37
Oscar Ramirez <osramirezf@gmail.com>
2023-10-29 15:19:55 -0300

    feat: Se agrega route.post para '/register'

--- routers/users.js
+++ routers/users.js
@@ -45,6 +45,28 @@ router.post("/", async (req, res) => {
   res.send(user);
 });
 
+
+router.post("/register", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    //color:req.body.color,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+
+  user = await user.save();
+
+  if (!user) return res.status(400).send("The user cannot be created!");
+
+  res.send(user);
+});
+
 router.post("/login", async (req, res) => {
   const user = await User.findOne({
     email: req.body.email,
