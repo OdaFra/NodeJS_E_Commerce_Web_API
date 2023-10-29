d50e578dd2d6f3d34aa2492ecc5e2191df545c2f
Oscar Ramirez <osramirezf@gmail.com>
2023-10-29 15:51:16 -0300

    refactor: Aplicamos expresion regular para manejo de rutas.

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,18 +1,19 @@
-const { expressjwt: expressjwt }   = require('express-jwt');
+const { expressjwt: expressjwt } = require("express-jwt");
 
 function authJwt() {
-    const secret = process.env.secret;
-
-    return expressjwt({
-        secret,
-        algorithms:['HS256']
-    }).unless({
-        path:[
-            '/api/v1/users/login',
-            '/api/v1/users/register'
-
-        ]
-    })
+  const secret = process.env.secret;
+    const api = process.env.API_URL;
+  return expressjwt({
+    secret,
+    algorithms: ["HS256"],
+  }).unless({
+    path: [
+      { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
+      { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
+    //   { url: `${api}/login`, methods: ["GET", "OPTIONS"] },
+    //   { url: `${api}/register`, methods: ["POST", "OPTIONS"] },
+    ],
+  });
 }
 
-module.exports = authJwt
+module.exports = authJwt;
