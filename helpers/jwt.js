0248648889b1f021d1a9a24df464ff3696eb3b09
Oscar Ramirez <osramirezf@gmail.com>
2023-11-02 12:07:45 -0300

    feat: Agregamos funcion para validar isAdmin como tambien exponer rutas de login and register.

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -2,18 +2,26 @@ const { expressjwt: expressjwt } = require("express-jwt");
 
 function authJwt() {
   const secret = process.env.secret;
-    const api = process.env.API_URL;
+  const api = process.env.API_URL;
   return expressjwt({
     secret,
     algorithms: ["HS256"],
+    isRevoked: isRevoked
   }).unless({
     path: [
       { url: /\/api\/v1\/products(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/api\/v1\/categories(.*)/, methods: ["GET", "OPTIONS"] },
-    //   { url: `${api}/login`, methods: ["GET", "OPTIONS"] },
-    //   { url: `${api}/register`, methods: ["POST", "OPTIONS"] },
+      {url:`${api}/users/login`, methods:['POST', 'OPTIONS']},
+      {url:`${api}/users/register`, methods:['POST', 'OPTIONS']},
+    //   `${api}/users/register`,
     ],
   });
+}
+async function isRevoked(req, payload, done){
+    if(!payload.isAdmin){
+        done(null, true)
+    }
+    done();
 }
 
 module.exports = authJwt;
